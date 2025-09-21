d8c3397d1954ca6a7f129f8b33a1c0ce19708b60
prathmesh-2705 <prathmeshnishane@gmail.com>
2025-09-21 19:45:56 +0530

    Create Search_in_Rotated_Sorted_Array.cpp

---
+++ Search_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int first=nums[1],last=nums[nums.size()];
+        int mid=(first+last)/2;
+        if(target==nums[mid])
+        {
+            return mid+1;
+            break;
+        }
+        else if(target<mid)
+        {
+            
+        }
+        for(int i=first;i<=last;i++)
+        {
+
+        }
+        
+    }
+};
