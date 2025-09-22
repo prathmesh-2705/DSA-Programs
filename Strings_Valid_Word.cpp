569cd77bc77215add7d8f21111a737e186bdbaae
Prathmesh Nishane <prathmeshnishane@gmail.com>
2025-09-22 23:25:58 +0530

    Update Strings_Valid_Word.cpp

--- Strings_Valid_Word.cpp
+++ Strings_Valid_Word.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        int size=s.size();
-        int st=0;
-        int en=size-1;
-        int i=0;
-        while(st<en)
+ bool isValid(string word) {
+        if(word.length()<3)
         {
-            char temp=s[en];
-            s[en]=s[st];
-            s[st]=temp;
-            st++;
-            en--;
+            return false;
         }
+        for (int i = 0; i < word.length(); i++) 
+        {
+            if (!isalnum(word[i])) {
+                return false;
+            }
+        }
+        for(int i=0;i<word.length();i++)
+        {
+            if(isVowel(word)==true && isConsonant(word)==true)
+            {
+                return true;
+            }
+        }  
+         return 0;
     }
+bool isVowel(string word)
+    {
+    for(int i=0;i<word.length();i++)
+    {
+        word[i]=tolower(word[i]);
+        if(word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u')
+        {
+            return true;
+        }
+
+    }  
+    return 0;
+}
+bool isConsonant(string word)
+    {
+    for(int i=0;i<word.length();i++)
+    {
+        word[i]=tolower(word[i]);
+        if(isalpha(word[i]) && word[i]!='a' && word[i]!='e' && word[i]!='i' && word[i]!='o' && word[i]!='u')
+        {
+            return true;
+        }
+    }  
+    return false;
+}
+   
 };
